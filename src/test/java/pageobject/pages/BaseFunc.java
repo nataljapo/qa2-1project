ffa8f09a4242743d5bfba50942dbfa4ffe25300f
Natalja <test@test.lv>
2022-03-16 17:23:31 +0300

    Logging

--- src/test/java/pageobject/pages/BaseFunc.java
+++ src/test/java/pageobject/pages/BaseFunc.java
@@ -1,5 +1,7 @@
 package pageobject.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,7 +17,10 @@ public class BaseFunc {
     private WebDriver browser;
     private WebDriverWait wait;
 
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
+
     public BaseFunc() {
+        LOGGER.info("Starting Web Browser");
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
@@ -23,6 +28,7 @@ public class BaseFunc {
     }
 
     public void openUrl(String url) {
+        LOGGER.info("Open page by: " + url);
         if (!url.startsWith("http://") && !url.startsWith("https://")) {
             url = "http://" + url;
         }
@@ -30,60 +36,78 @@ public class BaseFunc {
     }
 
     public void click(By locator) {
+        LOGGER.info("Clicking on element by locator " + locator);
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
 
     public void click(WebElement webElement) {
+        LOGGER.info("Clicking on web element");
         wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
     }
 
     public List<WebElement> findElements(By locator) {
+        LOGGER.info("Getting list of web elements by locator " + locator);
         return browser.findElements(locator);
     }
 
     public List<WebElement> findElements(WebElement parent, By child) {
+        LOGGER.info("Getting list of child elements");
         return parent.findElements(child);
     }
 
     public String getText(WebElement parent, By child) {
+        LOGGER.info("Getting text for child element");
         wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(parent, child));
         return parent.findElement(child).getText();
     }
 
     public String getText(By locator) {
+        LOGGER.info("Getting text for web element by locator " + locator);
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         return browser.findElement(locator).getText();
     }
 
     public void closeBrowser() {
+        LOGGER.info("Closing browser window");
         browser.close();
     }
 
     public WebElement findElement(By locator) {
+        LOGGER.info("Getting web element by locator " + locator);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public  WebElement findElement (WebElement parent, By child) {
+        LOGGER.info("Getting child element");
         return parent.findElement(child);
     }
 
     public void select(By locator, String value) {
+        LOGGER.info("Selecting " + value + " from dropdown by locator " + locator);
         Select select = new Select(findElement(locator));
         select.selectByValue(value);
     }
 
     public void selectByVisibleText(By locator, String text) {
+        LOGGER.info("Selecting " + text + " from dropdown by locator " + locator);
         Select select = new Select(findElement(locator));
         select.selectByVisibleText(text);
     }
 
     public void type(By locator, String text) {
+        LOGGER.info("Typing " + text + " in " + locator);
         WebElement inputField = findElement(locator);
         inputField.clear();
         inputField.sendKeys(text);
     }
 
     public void type(By locator, int text) {
+        LOGGER.info("Typing " + text + " in " + locator);
         type(locator, String.valueOf(text));
+    }
+
+    public void waitUntilElementsCountAtLeast(By locator, int count) {
+        LOGGER.info("Waiting for the number of elements to be more than " + count);
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(locator, count));
     }
 }
---
+++ src/test/java/stepdefs/TicketsStepDefs.java
@@ -0,0 +1,159 @@
+package stepdefs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import model.Reservation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import pageobject.pages.BaseFunc;
+import requesters.ReservationsRequester;
+import tickets.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class TicketsStepDefs {
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
+
+    private List<Reservation> response;
+    private Reservation givenReservation = new Reservation();
+
+    private BaseFunc baseFunc = new BaseFunc();
+    private HomePage homePage;
+    private PersonalInfoPage infoPage;
+    private SeatSelectionPage seatSelectionPage;
+    private SuccessfulBookingPage successfulBookingPage;
+
+    @Given("airports {string} and {string}")
+    public void set_airports(String from, String to) {
+        LOGGER.info("Setting airports");
+        givenReservation.setFrom(from);
+        givenReservation.setTo(to);
+    }
+
+    @Given("seat number is {int}")
+    public void set_seat_nr(int seatNr) {
+        LOGGER.info("Setting seat Nr.");
+        givenReservation.setSeat(seatNr);
+    }
+
+    @Given("personal info is:")
+    public void set_personal_info(Map<String, String> params) {
+        LOGGER.info("Setting personal information");
+        givenReservation.setName(params.get("first_name"));
+        givenReservation.setSurname(params.get("last_name"));
+        givenReservation.setDiscount(params.get("discount"));
+        givenReservation.setAdultCount(Integer.parseInt(params.get("adults")));
+        givenReservation.setChildren(Integer.parseInt(params.get("kids")));
+        givenReservation.setBagCount(Integer.parseInt(params.get("bags")));
+        givenReservation.setFullFlightDay(params.get("flight"));
+    }
+
+    @Given("home page opened")
+    public void open_home_page() {
+        LOGGER.info("Opening home page");
+        baseFunc.openUrl("http://www.qaguru.lv:8089/tickets/");
+        homePage = new HomePage(baseFunc);
+    }
+
+    @When("we are selecting airports")
+    public void select_airports() {
+        LOGGER.info("Selecting airports");
+        homePage.selectAirports(givenReservation.getFrom(), givenReservation.getTo());
+    }
+
+    @When("pressing GoGoGo button")
+    public void press_go_button() {
+        LOGGER.info("Pressing GoGoGo button");
+        homePage.pressGoBtn();
+        infoPage = new PersonalInfoPage(baseFunc);
+    }
+
+    @When("we are filling in personal info")
+    public void fill_personal_info_form() {
+        LOGGER.info("Filling personal information");
+        infoPage.fillInPersonalInfoForm(givenReservation);
+    }
+
+    @When("we are submitting form")
+    public void submit_personal_info_form() {
+        LOGGER.info("Submitting registration form");
+        infoPage.submitForm();
+    }
+
+    @When("we are pressing Book btn")
+    public void press_book_btn() {
+        LOGGER.info("Pressing Book button");
+        infoPage.book();
+        seatSelectionPage = new SeatSelectionPage(baseFunc);
+    }
+
+    @When("selecting seat")
+    public void select_seat() {
+        LOGGER.info("Selecting seat");
+        seatSelectionPage.selectSeat(givenReservation.getSeat());
+    }
+
+    @When("we are making final book")
+    public void final_book() {
+        LOGGER.info("Making final booking");
+        seatSelectionPage.book();
+        successfulBookingPage = new SuccessfulBookingPage(baseFunc);
+    }
+
+    @When("we are requesting all reservations")
+    public  void request_reservations() throws JsonProcessingException {
+        LOGGER.info("Requesting all reservations");
+        ReservationsRequester requester = new ReservationsRequester();
+        response = requester.getReservations();
+    }
+
+    @Then("selected airports appears")
+    public void check_selected_airports() {
+        LOGGER.info("Checking if correct airports appears");
+        List<String> selectedAirports = infoPage.getSelectedAirports();
+        Assertions.assertEquals(givenReservation.getFrom(), selectedAirports.get(0), "Wrong from!");
+        Assertions.assertEquals(givenReservation.getTo(), selectedAirports.get(1), "Wrong to!");
+    }
+
+    @Then("passenger name is shown")
+    public void check_passenger_name() {
+        LOGGER.info("Checking if a correct passenger name appears");
+        Assertions.assertEquals(givenReservation.getName(), infoPage.getPassengerName(), "Wrong name!");
+    }
+
+    @Then("correct seat selected")
+    public  void check_seat_nr() {
+        LOGGER.info("Checking if a correct seat selected");
+        Assertions.assertEquals(givenReservation.getSeat(), seatSelectionPage.getSelectedSeatNr(), "Wrong seat Nr!");
+    }
+
+    @Then("successful message appears")
+    public void check_success_msg() {
+        LOGGER.info("Checking if a successful message appears");
+        Assertions.assertTrue(successfulBookingPage.isSuccessMessagePresents(), "There is no success message!");
+    }
+
+    @Then("reservation exists in the list with correct data")
+    public  void check_current_reservation() {
+        LOGGER.info("Checking if a reservation exists in the list with correct data");
+        Reservation actualReservation = null; //=null
+        for (Reservation r : response) {
+            if (r.getName().equals(givenReservation.getName())) {
+                actualReservation = r;
+                break;
+            }
+        }
+        Assertions.assertNotNull(actualReservation, "There is no reservation!");
+        Assertions.assertEquals(givenReservation.getSurname(), actualReservation.getSurname(), "Incorrect surname!");
+        Assertions.assertEquals(givenReservation.getBagCount(), actualReservation.getBagCount(), "Incorrect bag count!");
+        Assertions.assertEquals(givenReservation.getDiscount(), actualReservation.getDiscount(), "Incorrect discount!");
+        Assertions.assertEquals(givenReservation.getChildren(), actualReservation.getChildren(), "Wrong children count!");
+        Assertions.assertEquals(givenReservation.getFlightDay(), actualReservation.getFlightDay(), "Wrong flight date!");
+        Assertions.assertEquals(givenReservation.getAdultCount(), actualReservation.getAdultCount(), "Wrong adult count!");
+        Assertions.assertEquals(givenReservation.getSeat(), actualReservation.getSeat(), "Wrong seat!");
+    }
+}
