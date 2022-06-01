fb5162942131ea75550198c2d6a463dedc6c3b29
Natalja <test@test.lv>
2022-06-01 17:56:54 +0300

    Selenium practice

--- src/test/java/selenium/FirstSeleniumTest.java
+++ src/test/java/selenium/FirstSeleniumTest.java
@@ -9,15 +9,12 @@ public class FirstSeleniumTest {
 
     @Test
     public void firstTvNetTest() {
-        //Setting path where driver
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-        //Opening browser window. Variable browser will contain Browser Window
         WebDriver browser = new ChromeDriver();
         browser.manage().window().maximize();
 
         browser.get("http://tvnet.lv");
 
-        //Some locators example sorted by priority
         By acceptButton = By.id("button");
         By emailInput = By.name("user{email}");
         By xpathButton = By.xpath(".//button[@mode = 'primary']");
